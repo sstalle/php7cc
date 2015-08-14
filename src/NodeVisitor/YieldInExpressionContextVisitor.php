daa9f64150e4fbea644c780f3d04c2e89736c7b1
alexey <smbddd@gmail.com>
2015-08-14 21:26:37 +0300

    Do not output warning messages about parenthesized yield expressions

--- src/NodeVisitor/YieldInExpressionContextVisitor.php
+++ src/NodeVisitor/YieldInExpressionContextVisitor.php
@@ -6,39 +6,35 @@ use PhpParser\Node;
 
 class YieldInExpressionContextVisitor extends AbstractVisitor
 {
+
     /**
      * @var \SplStack
      */
     protected $expressionStack;
-
-    protected $ignoredExpressionClasses = array(
-        'PhpParser\\Node\\Expr\\Assign',
-        'PhpParser\\Node\\Expr\\AssignRef',
-        'PhpParser\\Node\\Expr\\AssignOp',
-    );
 
     public function beforeTraverse(array $nodes)
     {
         $this->expressionStack = new \SplStack();
     }
 
-
     public function enterNode(Node $node)
     {
         if ($node instanceof Node\Expr\Yield_) {
-            if (!$this->expressionStack->isEmpty()) {
+            $startTokenPosition = $node->getAttribute('startTokenPos');
+            $endTokenPosition = $node->getAttribute('endTokenPos');
+
+            if (!(
+                    $this->isImmediateSiblingOfToken($startTokenPosition, true, '(')
+                    && $this->isImmediateSiblingOfToken($endTokenPosition, false, ')')
+                )
+                && !$this->expressionStack->isEmpty()
+            ) {
                 $this->addContextMessage(
                     '"yield" usage in expression context',
                     $this->expressionStack->top()
                 );
             }
         } elseif ($node instanceof Node\Expr) {
-            foreach ($this->ignoredExpressionClasses as $ignoredClass) {
-                if (is_a($node, $ignoredClass)) {
-                    return;
-                }
-            }
-
             $this->expressionStack->push($node);
         }
     }
@@ -50,5 +46,27 @@ class YieldInExpressionContextVisitor extends AbstractVisitor
         }
     }
 
+    /**
+     * Returns true if $sourceTokenPosition is separated from $targetToken by whitespace tokens only,
+     * otherwise false
+     *
+     * @param string $sourceTokenPosition Position in the token array to start search from
+     * @param bool $before true - search before source token, false - after
+     * @param string $targetToken Token value to search for
+     * @return bool
+     */
+    protected function isImmediateSiblingOfToken($sourceTokenPosition, $before, $targetToken)
+    {
+        while (isset($this->tokens[$before ? --$sourceTokenPosition : ++$sourceTokenPosition])) {
+            $currentToken = $this->tokens[$sourceTokenPosition];
+            if (is_array($currentToken) && preg_match('/\s+/', $currentToken[1]) === 1) {
+                continue;
+            }
+
+            return $currentToken === $targetToken;
+        }
+
+        return false;
+    }
 
 }
