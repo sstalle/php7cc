76b9858477a5b77776d77592915bb443b04b56c8
alexey <smbddd@gmail.com>
2015-08-31 18:02:34 +0300

    Detect $HTTP_RAW_POST_DATA access through $GLOBALS array

--- src/NodeVisitor/HTTPRawPostDataVisitor.php
+++ src/NodeVisitor/HTTPRawPostDataVisitor.php
@@ -10,7 +10,15 @@ class HTTPRawPostDataVisitor extends AbstractVisitor
 
     public function enterNode(Node $node)
     {
-        if ($node instanceof Node\Expr\Variable && $node->name === static::HTTP_RAW_POST_DATA_VARIABLE_NAME) {
+        $isVariableAccessedByName = $node instanceof Node\Expr\Variable
+            && $node->name === static::HTTP_RAW_POST_DATA_VARIABLE_NAME;
+        $isVariableAccessedThroughGlobals = $node instanceof Node\Expr\ArrayDimFetch
+            && $node->var instanceof Node\Expr\Variable
+            && $node->var->name == 'GLOBALS'
+            && $node->dim instanceof Node\Scalar\String_
+            && $node->dim->value === static::HTTP_RAW_POST_DATA_VARIABLE_NAME;
+
+        if ($isVariableAccessedByName || $isVariableAccessedThroughGlobals) {
             $this->addContextMessage(
                 sprintf(
                     'Removed "%s" variable used',
