8ec80666cb498ad8bd82dc668ad3a968d76fd7a7
alexey <smbddd@gmail.com>
2015-08-31 18:29:26 +0300

    Add executable shell script

--- bin/php7cc.php
+++ bin/php7cc.php
@@ -1,7 +1,21 @@
 <?php
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__ . '/../vendor/autoload.php';
+$autoloadFiles = array(
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+);
+
+$loader = null;
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        $loader = require $autoloadFile;
+        break;
+    }
+}
+
+if (!$loader) {
+    exit('Autoloader not found. Try installing dependencies using composer install.');
+}
 
 $app = new \Sstalle\php7cc\Infrastructure\Application();
 $app->run();
