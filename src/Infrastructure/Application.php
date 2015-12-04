7f25940777582f192717868ad823017127b52901
alexey <smbddd@gmail.com>
2015-12-04 20:56:08 +0300

    v1.0.2

--- src/Infrastructure/Application.php
+++ src/Infrastructure/Application.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 
 class Application extends \Symfony\Component\Console\Application
 {
-    const VERSION = '1.0.2-dev';
+    const VERSION = '1.0.2';
 
     /**
      * {@inheritdoc}
